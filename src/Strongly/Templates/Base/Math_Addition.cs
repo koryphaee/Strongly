9ff50ba7461b33b754e412def4f4cc997430d04a
koryphaee <coding@juliusmann.com>
2024-11-04 19:59:48 +0100

    fix for (u)short and (s)byte math

--- src/Strongly/Templates/Base/Math_Addition.cs
+++ src/Strongly/Templates/Base/Math_Addition.cs
@@ -1,1 +1,1 @@
-﻿    public static TYPENAME operator +(TYPENAME a, TYPENAME b) => new TYPENAME(a.Value + b.Value);
+﻿    public static TYPENAME operator +(TYPENAME a, TYPENAME b) => new TYPENAME((BASE_TYPENAME) (a.Value + b.Value));
